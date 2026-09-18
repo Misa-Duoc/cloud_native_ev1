398f010c2e6d98242979cbb8b190f5ab943e7f19
Misa <misa.fuentes@duocuc.cl>
2026-09-18 15:44:43 -0300

    Corregido authconfig

--- frontend/dfs-s3-login-1/src/authConfig.js
+++ frontend/dfs-s3-login-1/src/authConfig.js
@@ -3,7 +3,7 @@ export const msalConfig = {
         clientId: "3cbc0426-09ed-4e59-b1ff-60981daa8762",
 
         authority:
-            "https://login.microsoftonline.com/c2154212-c473-414a-9t88-8860491a518c",
+            "https://login.microsoftonline.com/c2154212-c473-414a-9f88-8860491a518c",
 
         redirectUri: "http://localhost:3000/"
     },
